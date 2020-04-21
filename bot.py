119678fe808e24c318816daf41f39cb8dbcc2558
AhmadNaruto <47007634+AhmadNaruto@users.noreply.github.com>
2020-04-21 18:45:00 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,22 @@
 import telebot
 import os
-import pornhub
 from flask import Flask,request
+from pornhub_api import PornhubApi
+
+api = PornhubApi()
+def cari(kata):
+   tags = random.sample(api.video.tags(kata).tags, 5)
+   category = random.choice(api.video.categories().categories)
+   return result = api.search.search(ordering="mostviewed", tags=tags, category=category)
+
+#print(result.size())
+#for vid in result.videos:
+#    print(vid.title, vid.url)
+
 
 TOKEN = "1171359990:AAFwhEhDSLdCWIFM7lSz0clIUoUTP8erW-w"
 bot = telebot.TeleBot(token=TOKEN)
 server = Flask(__name__)
-
-search_keywords = []
-
-#client = pornhub.PornHub("5.135.164.72", 3128, search_keywords)
-#With proxy, given a Proxy IP and Port. For the countries with restricted access like Turkey, etc.
-
-client = pornhub.PornHub(search_keywords)
-def cari(kata):   
-    for video in client.getVideos(10,page=2):
-    #print(video)
-    
-    for photo_url in client.getPhotos(5):
-    #print(photo_url)
-    return video, photo_url
 
 @bot.message_handler(commands=['start']) # welcome message handler
 def send_welcome(message):
@@ -32,7 +29,8 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
         h = cari(message.text)
-	bot.reply_to(message, h)
+        for co in h.result:
+	   bot.reply_to(message, co.url)
 
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
