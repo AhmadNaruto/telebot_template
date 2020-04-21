6de800617e2baf1408b51529c692200a5a18ce9c
AhmadNaruto <47007634+AhmadNaruto@users.noreply.github.com>
2020-04-21 18:04:55 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,25 @@
 import telebot
 import os
+import pornhub
 from flask import Flask,request
 
 TOKEN = "1171359990:AAFwhEhDSLdCWIFM7lSz0clIUoUTP8erW-w"
 bot = telebot.TeleBot(token=TOKEN)
 server = Flask(__name__)
+
+search_keywords = []
+
+#client = pornhub.PornHub("5.135.164.72", 3128, search_keywords)
+#With proxy, given a Proxy IP and Port. For the countries with restricted access like Turkey, etc.
+
+client = pornhub.PornHub(search_keywords)
+def cari(kata):   
+    for video in client.getVideos(10,page=2):
+    #print(video)
+    
+    for photo_url in client.getPhotos(5):
+    #print(photo_url)
+    return video, photo_url
 
 @bot.message_handler(commands=['start']) # welcome message handler
 def send_welcome(message):
@@ -16,7 +31,8 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
+        h = cari(message.text)
+	bot.reply_to(message, h)
 
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
